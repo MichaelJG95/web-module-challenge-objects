dbd5f4b0a7dfd692c2272a66f5b11114ea6ec3ce
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-01-13 15:13:52 -0600

    commit for Task 7 completion with Stretch 1

--- index.js
+++ index.js
@@ -163,10 +163,17 @@ Use the getReviewsByRating function below to do the following:
   ]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
+ function getReviewByRating(array, rating) {
+    const inRange = [];
+    for(let i = 0; i < array.length; i++){
+      if (array[i].rating >= rating && array[i].rating < rating + 1){
+        inRange.push(array[i]);
+      }
+    }
+    return inRange;
   }
 
+  console.log(getReviewByRating(reviews, 4));
   
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪💪💪💪💪   
 Use the getLongReviews function below to do the following:
